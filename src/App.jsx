c8f591f43e91e5c75516e17911e93da654af5023
xxedrixx <cedric.sefo40@gmail.com>
2023-11-24 22:56:39 -0700

    set up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="sm:px-20 px-10 py-20">Hero</section>
+    <section className="sm:px-20 px-10 py-20">Sports</section>
+    <section className="sm:px-20 px-10 py-20">Services</section>
+    <section className="sm:px-20 px-10 py-20">About</section>
+    <section className="sm:px-20 px-10 py-20">Contact</section>
+  </main>
+);
+
+export default App;
