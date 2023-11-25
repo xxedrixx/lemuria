a0e13c2c595d366677ef4e89e9ae053a0e64ef61
xxedrixx <cedric.sefo40@gmail.com>
2023-11-24 23:31:37 -0700

    sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import { Hero, Contact, About, Footer, Services, Sports } from "./sections";
+
 const App = () => (
   <main className="relative">
-    Nav
-    <section className=" px-20 py-20">Hero</section>
-    <section className=" px-20 py-20">Sports</section>
-    <section className=" px-20 py-20">Services</section>
-    <section className=" px-20 py-20">About</section>
-    <section className=" px-20 py-20">Contact</section>
-    <section className=" px-20 py-20 bg-lemuria">Footer</section>
+    {/* <Nav /> */}
+    <section className=" px-20 py-20">
+      <Hero />
+    </section>
+    <section className=" px-20 py-20">
+      <Sports />
+    </section>
+    <section className=" px-20 py-20">
+      <Services />
+    </section>
+    <section className=" px-20 py-20">
+      <About />
+    </section>
+    <section className=" px-20 py-20">
+      <Contact />
+    </section>
+    <section className=" px-20 py-20 bg-lemuria">
+      <Footer />
+    </section>
   </main>
 );
 
---
+++ src/sections/index.js
@@ -0,0 +1,8 @@
+import Hero from "./Hero";
+import About from "./About";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Services from "./Services";
+import Sports from "./Sports";
+
+export { Hero, About, Contact, Footer, Services, Sports };
