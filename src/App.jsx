250ded69f7ecaf039f370361500e78f679c8e309
xxedrixx <cedric.sefo40@gmail.com>
2023-11-24 23:20:03 -0700

    custom color

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 const App = () => (
   <main className="relative">
     Nav
-    <section className="sm:px-20 px-10 py-20">Hero</section>
-    <section className="sm:px-20 px-10 py-20">Sports</section>
-    <section className="sm:px-20 px-10 py-20">Services</section>
-    <section className="sm:px-20 px-10 py-20">About</section>
-    <section className="sm:px-20 px-10 py-20">Contact</section>
+    <section className=" px-20 py-20">Hero</section>
+    <section className=" px-20 py-20">Sports</section>
+    <section className=" px-20 py-20">Services</section>
+    <section className=" px-20 py-20">About</section>
+    <section className=" px-20 py-20">Contact</section>
+    <section className=" px-20 py-20 bg-lemuria">Footer</section>
   </main>
 );
 
