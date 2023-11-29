260babbb938cee4f552264b8eee81950d1a76aaf
xxedrixx <cedric.sefo40@gmail.com>
2023-11-29 14:32:19 -0700

    nav label color

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -40,7 +40,7 @@ const Nav = () => {
             <li key={item.label}>
               <a
                 href={item.href}
-                className="font-montserrat leading-normal text-lg text-slate-gray"
+                className="font-montserrat leading-normal text-lg"
               >
                 {item.label}
               </a>
