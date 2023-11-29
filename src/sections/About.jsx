0aa3c2be4d665a968ffa09a17d21b9ee4bc225f2
xxedrixx <cedric.sefo40@gmail.com>
2023-11-29 13:21:55 -0700

    about

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,5 +1,15 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="max-container ">
+      <h1 className="flex justify-center items-center">ABOUT</h1>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos eos
+        earum accusamus quam vitae delectus distinctio maxime accusantium
+        doloribus libero dolorem soluta quasi sed nemo, laboriosam assumenda
+        molestias, tempora provident.
+      </div>
+    </section>
+  );
 };
 
 export default About;
