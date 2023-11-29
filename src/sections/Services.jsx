87d92b8aa75a3056cf4d5c3ac3a6082c0c425821
xxedrixx <cedric.sefo40@gmail.com>
2023-11-29 15:15:17 -0700

    services

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -4,7 +4,9 @@ import ServiceCard from "../components/ServiceCard";
 const Services = () => {
   return (
     <section id="services" className="max-container ">
-      <h1 className="flex justify-center items-center">SERVICES</h1>
+      <h1 className="flex justify-center items-center font-palanquin text-4xl capitalize font-bold mb-5 text-lemuria">
+        SERVICES
+      </h1>
       <div className="flex justify-center flex-wrap gap-9">
         {services.map((service) => (
           <ServiceCard key={service.label} {...service} />
