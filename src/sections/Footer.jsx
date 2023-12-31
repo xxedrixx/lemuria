cdb48ba8fbabce00cb05455dc72b5d78d17c43e0
xxedrixx <cedric.sefo40@gmail.com>
2023-12-30 18:04:52 -0700

    footer page content

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -6,17 +6,20 @@ const Footer = () => {
     <footer className="max-container flex justify-around max-[730px]:flex-col">
       <div className="max-lg:mb-[20px]">
         <div className="flex justify-center items-center font-montserrat text-2xl leading-normal font-medium mb-1">
-          Contact Us
+          Contactez-nous
         </div>
-        <div className="flex justify-center items-center font-montserrat text-base leading-normal text-white">
-          <a href="mailto:lemuria@lemuria.mg" className="underline">
-            lemuria@lemuria.mg
+        <div className="flex flex-col justify-center items-center font-montserrat text-base leading-normal text-white ">
+          <a href="mailto:contact@lemuria.mg" className="underline">
+            contact@lemuria.mg
+          </a>
+          <a href="tel:+261383149191" className="underline">
+            +261 38 31 491 91
           </a>
         </div>
       </div>
       <div className="max-lg:mb-[10px]">
         <div className="flex justify-center items-center font-montserrat text-2xl leading-normal font-medium mb-1">
-          Our location
+          Emplacement
         </div>
         <div className="flex justify-center items-center font-montserrat text-base leading-normal text-white">
           Antananarivo, Madagascar
@@ -24,10 +27,14 @@ const Footer = () => {
       </div>
       <div className="max-lg:mb-[10px]">
         <div className="flex justify-center items-center font-montserrat text-2xl leading-normal font-medium mb-1">
-          Follow Us
+          Suivez-nous
         </div>
         <div className="flex justify-center items-center gap-4">
-          <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
+          <a
+            href="https://www.facebook.com/people/Lemuria-Sport-Association/100090428749662/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaFacebook size={30} color="white" />
           </a>{" "}
           <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
