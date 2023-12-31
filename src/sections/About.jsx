8a59768b13d34c25530c5170828b87044e8aef35
xxedrixx <cedric.sefo40@gmail.com>
2023-12-30 17:53:14 -0700

    about page content

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,13 +2,13 @@ const About = () => {
   return (
     <section id="about" className="max-container ">
       <h1 className="flex justify-center items-center font-palanquin text-4xl capitalize font-bold mb-5 text-lemuria">
-        ABOUT
+        A PROPOS
       </h1>
       <div className="break-words font-montserrat text-lg leading-7">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos eos
-        earum accusamus quam vitae delectus distinctio maxime accusantium
-        doloribus libero dolorem soluta quasi sed nemo, laboriosam assumenda
-        molestias, tempora provident.
+        Regis par la loi 97-014 Lemuria est une association sportive trouvant
+        son origine à Madagascar en 2022, dont l&#39;objectif ultime de
+        l&#39;association est de promouvoir la relation humaine à travers la
+        pratique sportive.
       </div>
     </section>
   );
