fc4af8a65decb9cc0a59ecbac367419246bfaf6a
xxedrixx <cedric.sefo40@gmail.com>
2024-01-23 14:28:15 -0700

    typo

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -15,8 +15,8 @@ const Hero = () => {
       <div className="relative flex-1 flex flex-col justify-center items-start xl:min-h-screen w-full max-xl:padding-x font-montserrat text-lg leading-7">
         <div className="mb-5 break-words">
           Lemuria est une association sportive trouvant son origine à Madagascar
-          en 2022, dont l&#39;objectif ultime de l&#39;association est de
-          promouvoir la relation humaine à travers la pratique sportive.
+          en 2022, dont l&#39;objectif ultime est de promouvoir la relation
+          humaine à travers la pratique sportive.
         </div>
         <div>
           <Button />
